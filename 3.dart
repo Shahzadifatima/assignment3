a066690eba20c9d56399eb8f1ddb8414090e5c80
bintemasood <humairamasood19@gmail.com>
2024-03-28 11:55:42 -0400

    assignment3

---
+++ 3.dart
@@ -0,0 +1,33 @@
+void main (){
+//Q.1:
+int length = 10 ;
+int breadth = 5;
+if (length==breadth){
+  print ("it's a Square");
+} else {
+  print ("it's a Rectangle");
+}
+
+//Q.2:
+int abc = 47;
+int def = 36;
+if(abc>def){
+  print("zeeshan is oldest");
+  print("fatima is youngest");
+} else {
+  print("zeeshan is youngest");
+  print ("fatima is oldest");
+}
+
+//Q.3:
+var num = 20; 
+   if(num > 0) { 
+      print("${num} is positive"); 
+   } 
+   else if(num < 0) { 
+      print("${num} is negative"); 
+   } else { 
+      print("${num} is neither positive nor negative"); 
+   } 
+
+}
